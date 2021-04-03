232b8589f3980a7d73d765960c98644c777c6d17
energyandpublichealth-proj <69124745+energyandpublichealth-proj@users.noreply.github.com>
2021-04-03 01:42:51 -0700

    Delete EnergyandPublicHealth directory

--- EnergyandPublicHealth/ComparativeImpactsProject/vidPage/index.js
+++
@@ -1,9 +0,0 @@
-document.onmousemove = function(){
-	var count = 0;
-	count+=1;
-	// console.log(count);
-	if(count = 1){
-		console.log("hey");
-			window.location.replace("../index.html");
-	}
-};
